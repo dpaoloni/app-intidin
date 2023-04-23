9e18c31563063575df5700484a404936c80016dc
dpaoloni <denisepaoloni08@gmail.com>
2023-04-23 00:27:06 -0300

    experiencia

--- app-intidin/src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
+++ app-intidin/src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
@@ -9,12 +9,15 @@ import { PorfolioService } from 'src/app/servicios/porfolio.service';
   
 export class ExperienciaYEducacionComponent implements OnInit {
   educacionLista: any;
+  experienciaLista: any;
 
   constructor(private datosPorfolio: PorfolioService){}
   
   ngOnInit(): void {
-    this.datosPorfolio.obtenerDatos().subscribe(data => {
-      this.educacionLista = data.educacion; 
+    this.datosPorfolio.obtenerDatos().subscribe
+      (data => {
+        this.educacionLista = data.educacion; 
+        this.experienciaLista = data.experiencia;
     })
     }
 
